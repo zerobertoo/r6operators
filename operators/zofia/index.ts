c542b80013d9b9eb747f817f8f5b96bb1ffff8ef
Suspense <64612795+AlmostSuspense@users.noreply.github.com>
2026-06-09 00:13:46 +0200

    update Zofia's ratings (#1)
    Co-authored-by: José Roberto <109639857+zerobertoo@users.noreply.github.com>

--- operators/zofia/index.ts
+++ operators/zofia/index.ts
@@ -6,8 +6,8 @@ export const zofia: IOperator = {
   org: "GROM",
   squad: "GHOSTEYES",
   ratings: {
-    health: 3,
-    speed: 1,
+    health: 2,
+    speed: 2,
     difficulty: 1,
   },
   meta: {
